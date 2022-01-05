40fb1f4b26766334e335a1424c6b35981352c8fc
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-05 22:22:28 +0900

    ログイン機能

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/createUser.dto';
+import { CredentialsDto } from './dto/credensials.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,13 @@ export class AuthController {
 
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.authService.signUp(createUserDto)
+    return this.authService.signUp(createUserDto);
+  }
+
+  @Post('signin')
+  async signIn(
+    @Body() credentialsDto: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.signIn(credentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,35 @@
-import { Injectable } from '@nestjs/common';
-import { create } from 'domain';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { User } from 'src/entities/user.entity';
 import { CreateUserDto } from './dto/createUser.dto';
+import { CredentialsDto } from './dto/credensials.dto';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepository: UserRepository){}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
-    return await this.userRepository.createUser(createUserDto)
+    return await this.userRepository.createUser(createUserDto);
+  }
+
+  async signIn(
+    credentialsDTO: CredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = credentialsDTO;
+    const user = await this.userRepository.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload = { id: user.id, username: user.username };
+      const accessToken = await this.jwtService.sign(payload);
+      return { accessToken };
+    }
+    throw new UnauthorizedException(
+      'ユーザー名またはパスワードを確認してください',
+    );
   }
 }
